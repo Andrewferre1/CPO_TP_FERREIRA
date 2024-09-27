9936ce9b1805b9402342751d3a20397e8a07496f
Andrew ferreira <andrew.ferreira@epfedu.fr>
2024-09-27 11:10:40 +0200

    Création d'un nouveau projet versionné

---
+++ TP1/Exo1/src/exo1/Tp1Exo1.java
@@ -0,0 +1,40 @@
+/*Andrew
+ * TP1_manipNombreslnt_Andrew ferreira
+ * 
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ferre
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    int nbr1;
+    int nbr2;
+    Scanner sc = new Scanner(System.in);
+    nbr1 =sc.nextInt();
+    nbr2= sc.nextInt();
+    System.out.println(nbr1+nbr2+" voici la somme des deux nombre");
+    System.out.println(nbr1-nbr2+" voici la différence des deux nombre");
+    System.out.println(nbr1*nbr2+" Voici le rpoduit des deux nombre");
+    
+    System.out.println(nbr1/nbr2);
+    System.out.println(nbr1%nbr2);
+    
+    
+    
+    
+             
+    
+        // TODO code application logic here
+    }
+    
+}
