76ec685e9149af8f3c69f35ee7115ef65bad3b27
Andrew ferreira <andrew.ferreira@epfedu.fr>
2024-10-16 10:09:43 +0200

    Création d'un nouveau projet versionné

---
+++ TP0/exo2/src/exo2/Exo2.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ferre
+ */
+public class Exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("bonjour");
+        
+        System.out.println("aurevoir");
+    String prenom;
+    Scanner sc;
+    sc = new Scanner(System.in);
+    System.out.println("Bonjour, quel est votre prenom ?");
+    prenom = sc.nextLine();
+        
+                // TODO code application logic here
+;
+    
+        
+    }
+    
+}
