9705f1a96759abea6b38c9bc7f8b5cb3474e60e9
Andrew ferreira <andrew.ferreira@epfedu.fr>
2024-10-16 10:09:59 +0200

    Création d'un nouveau projet versionné

---
+++ TP0/exercice1/src/exercice1/Exercice1.java
@@ -0,0 +1,21 @@
+/*
+ *Andrew fereira
+ * 
+ */
+package exercice1;
+
+/**
+ *
+ * @author ferre
+ */
+public class Exercice1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bonjour");
+    }
+    
+}
